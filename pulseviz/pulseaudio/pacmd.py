9292a4c571814f8bcf69185e46856db201155973
Patrick Bartels <pckbls@gmail.com>
2017-06-16 09:41:03 +0200

    Implement pacmd module

--- pulseviz/pulseaudio/pacmd.py
+++ pulseviz/pulseaudio/pacmd.py
@@ -1,3 +1,19 @@
-def get_sources():
-    pass
+import subprocess
+import re
 
+
+def list_sources():
+    result = []
+
+    process = subprocess.Popen(['pacmd', 'list-sources'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout, _ = process.communicate(timeout=10.0)
+    if process.returncode != 0:
+        raise Exception('pacmd exited with return code {0}'.format(process.returncode))
+
+    for line in stdout.splitlines(keepends=False):
+        line = line.decode('ascii')
+        match = re.match('^[\t]name: <(.*)>$', line)
+        if match is not None:
+            result.append(match.group(1))
+
+    return result
--- tests/pulseaudio/test_pacmd.py
+++ tests/pulseaudio/test_pacmd.py
@@ -1,8 +1,10 @@
 import pytest
 from pulseviz.pulseaudio import pacmd
-from .test_pulseaudio import fixture_pulseaudio_server
+from .test_pulseaudio import fixture_pulseaudio_server, fixture_null_sink
 
 
-def test_get_sources(fixture_pulseaudio_server):
-    pacmd.get_sources()
-
+def test_list_sources(fixture_null_sink):
+    _, source_name = fixture_null_sink
+    result = pacmd.list_sources()
+    print(result)
+    assert source_name in result
