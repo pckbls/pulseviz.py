972af57e826d545e7318fd7a719197e44fa2ed53
Patrick Bartels <pckbls@gmail.com>
2017-06-18 19:16:16 +0200

    Make it possible to change axis settings for spectrum visualizer

--- pulseviz/visualizer/spectrum.py
+++ pulseviz/visualizer/spectrum.py
@@ -20,23 +20,51 @@ class SpectrumVisualizer(Visualizer):
         self.auto_scale = True
         self.max_observed_y = None
 
+        self.setup_x_axis('log')
+        self.setup_y_axis('log', -70, 70)
+    
+    def setup_x_axis(self, mode):
+        self.x_axis_mode = mode
+    
+    def setup_y_axis(self, mode, min_y, max_x):
+        self.y_axis_mode = mode
+        self.y_axis_min = min_y
+        self.y_axis_max = max_x
+
     def resize(self, width, height):
         super(SpectrumVisualizer, self).resize(width, height)
-        self.x_coordinates = (self.width+1) - np.logspace(np.log2(self.width+1),
-                                                          0,
-                                                          len(self.analyzer.fft_left_side),
-                                                          base=2)
+
+        if self.x_axis_mode == 'lin':
+            self.x_coordinates = np.linspace(0, self.width, len(self.analyzer.fft_left_side))
+        elif self.x_axis_mode == 'log':
+            self.x_coordinates = (self.width+1) \
+                    - np.logspace(np.log2(self.width+1),
+                                  0,
+                                  len(self.analyzer.fft_left_side),
+                                  base=2)
+        else:
+            raise Exception('Unknown option: {0}'.format(self.x_axis_mode))
 
     def display(self):
         with self.analyzer.fft_left_side_lock:
-            y_coordinates = self.analyzer.fft_left_side
+            if self.y_axis_mode == 'lin':
+                y_coordinates = self.analyzer.fft_left_side
+            elif self.y_axis_mode == 'log':
+                # TODO: Fix division-by-zero RuntimeWarnings.
+                y_coordinates = 20 * np.log10(self.analyzer.fft_left_side)
+            else:
+                raise Exception('Unknown option: {0}'.format(self.x_axis_mode))
 
-            if self.auto_scale:
-                m = np.max(y_coordinates)
-                if self.max_observed_y is None or m > self.max_observed_y:
-                    self.max_observed_y = m
-                if self.max_observed_y > 0.0:
-                    y_coordinates /= self.max_observed_y
+            y_coordinates = (y_coordinates - self.y_axis_min) / (self.y_axis_max - self.y_axis_min)
+
+            # TODO: Re-implement this properly.
+            if False:
+                if self.auto_scale:
+                    m = np.max(y_coordinates)
+                    if self.max_observed_y is None or m > self.max_observed_y:
+                        self.max_observed_y = m
+                    if self.max_observed_y > 0.0:
+                        y_coordinates /= self.max_observed_y
 
             glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
             glColor3f(0.25, 0.0, 1.0)
