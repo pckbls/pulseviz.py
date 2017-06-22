9662ff0dd2ca3b249785a0a26c16fe3bd0a13270
Patrick Bartels <pckbls@gmail.com>
2017-06-22 18:36:21 +0200

    Add --visualizer and --signal-generator options

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,6 +2,17 @@ import pytest
 from .pulseaudio.fake_simple_client import FakeSimpleRecordClient
 
 
+def pytest_addoption(parser):
+    parser.addoption('--visualizer',
+                     action='store',
+                     default=None,
+                     help='which visualizer type to choose for rendering tests')
+    parser.addoption('--signal-generator',
+                     action='store',
+                     default=None,
+                     help='which signal generator to choose for rendering tests')
+
+
 @pytest.fixture
 def fake_simple_record_client():
     yield FakeSimpleRecordClient(source='whatever')
