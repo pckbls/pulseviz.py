bf92e3783b0b9824eb9568f77641f8a86ee2e597
Patrick Bartels <pckbls@gmail.com>
2017-07-01 22:54:13 +0200

    Refactor waveform visualizer to use pyglet

--- pulseviz/visualizers/waveform.py
+++ pulseviz/visualizers/waveform.py
@@ -1,36 +1,38 @@
-import threading
-import numpy as np
-
-from OpenGL.GL import *
-from OpenGL.GLU import *
-from OpenGL.GLUT import *
-
-from . import visualizer
-from ..visualizer import Visualizer
-from ..opengl_window import OpenGLWindow2D
+import pyglet
+from pyglet import gl
+from . import visualizer, Visualizer, VisualizerWindow
 from ..dsp.sampler import Sampler
+
+
+class WaveformVisualizerWindow(VisualizerWindow):
+    def update(self, dt):
+        # TODO: Update vertex data here.
+        pass
+
+    def on_draw(self):
+        self.clear()
+
+        with self._analyzer.samples_lock:
+            bin_width = self.width / self._analyzer.sample_size
+
+            gl.glColor3f(0.25, 0.0, 1.0)
+            gl.glBegin(gl.GL_LINE_STRIP)
+            for i, height in enumerate(self._analyzer.samples):
+                x_min = i * bin_width
+                y = self._analyzer.samples[i] * self.height / 2 + self.height / 2
+                gl.glVertex3f(x_min, y, 0.0)
+            gl.glEnd()
 
 
 @visualizer(name='waveform')
 class WaveformVisualizer(Visualizer):
-    window_name = 'waveform - pulseviz'
+    VISUALIZER_WINDOW_TYPE = WaveformVisualizerWindow
+    WINDOW_TITLE = 'Waveform Visualizer'
 
-    def __init__(self, sample_size, pulseaudio_client, **kwargs):
-        super(WaveformVisualizer, self).__init__(**kwargs)
-        self.analyzer = Sampler(sample_size=sample_size, pulseaudio_client=pulseaudio_client)
+    def setup_analyzer(self):
+        self._analyzer = Sampler(pulseaudio_client=self._pulseaudio_client,
+                                 sample_size=4096)
 
-    def _display(self):
-        with self.analyzer.samples_lock:
-            bin_width = self.width / self.analyzer.sample_size
-
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-            glColor3f(0.25, 0.0, 1.0)
-
-            glBegin(GL_LINE_STRIP)
-            for i, height in enumerate(self.analyzer.samples):
-                x_min = i * bin_width
-                y = self.analyzer.samples[i] * self.height/2 + self.height/2
-                glVertex3f(x_min, y, 0.0)
-            glEnd()
-
-            glutSwapBuffers()
+    def start(self, **kwargs):
+        pyglet.clock.schedule_interval(self._window.update, 1 / 10)
+        super().start(**kwargs)
