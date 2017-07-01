9e6bdb0f936e85aedc07ff3914a968049ba79c7b
Patrick Bartels <pckbls@gmail.com>
2017-07-01 22:30:49 +0200

    Remove rendering tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,17 +2,6 @@ import pytest
 from .fake_simple_client import FakeSimpleRecordClient
 
 
-def pytest_addoption(parser):
-    parser.addoption('--visualizer',
-                     action='store',
-                     default=None,
-                     help='which visualizer type to choose for rendering tests')
-    parser.addoption('--signal-generator',
-                     action='store',
-                     default=None,
-                     help='which signal generator to choose for rendering tests')
-
-
 @pytest.fixture
 def fake_simple_record_client():
     yield FakeSimpleRecordClient(source='whatever')
