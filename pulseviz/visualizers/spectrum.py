6e22fa662463f23fcbaf0e422222be932494754f
Patrick Bartels <pckbls@gmail.com>
2017-07-01 22:54:23 +0200

    Refactor spectrum visualizer to use pyglet

--- pulseviz/visualizers/spectrum.py
+++ pulseviz/visualizers/spectrum.py
@@ -1,59 +1,54 @@
-import threading
 import numpy as np
-
-from OpenGL.GL import *
-from OpenGL.GLU import *
-from OpenGL.GLUT import *
-
-from . import visualizer
-from ..visualizer import Visualizer
-from ..opengl_window import OpenGLWindow2D
+import pyglet
+from pyglet import gl
+from . import visualizer, Visualizer, VisualizerWindow
 from ..dsp.fft import FFTAnalyzer
 
 
-@visualizer(name='spectrum')
-class SpectrumVisualizer(Visualizer):
-    window_name = 'spectrum - pulseviz'
-
-    def __init__(self, sample_size, pulseaudio_client, **kwargs):
-        super(SpectrumVisualizer, self).__init__(**kwargs)
-        self.analyzer = FFTAnalyzer(sample_size=sample_size,
-                                    pulseaudio_client=pulseaudio_client)
+class SpectrumVisualizerWindow(VisualizerWindow):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
         self.auto_scale = True
         self.max_observed_y = None
 
         self.setup_x_axis('log')
         self.setup_y_axis('log', -70, 70)
-    
+
     def setup_x_axis(self, mode):
         self.x_axis_mode = mode
-    
+
     def setup_y_axis(self, mode, min_y, max_x):
         self.y_axis_mode = mode
         self.y_axis_min = min_y
         self.y_axis_max = max_x
 
-    def _resize(self, width, height):
-        super(SpectrumVisualizer, self)._resize(width, height)
+    def update(self, dt):
+        # TODO: Update vertex data here.
+        pass
+
+    def on_resize(self, width, height):
+        super().on_resize(width, height)
 
         if self.x_axis_mode == 'lin':
-            self.x_coordinates = np.linspace(0, self.width, len(self.analyzer.fft))
+            self.x_coordinates = np.linspace(0, self.width, len(self._analyzer.fft))
         elif self.x_axis_mode == 'log':
-            self.x_coordinates = (self.width+1) \
-                    - np.logspace(np.log2(self.width+1),
-                                  0,
-                                  len(self.analyzer.fft),
-                                  base=2)
+            self.x_coordinates = self.width + 1
+            self.x_coordinates -= np.logspace(np.log2(self.width + 1),
+                                              0,
+                                              len(self._analyzer.fft),
+                                              base=2)
         else:
             raise Exception('Unknown option: {0}'.format(self.x_axis_mode))
 
-    def _display(self):
-        with self.analyzer.fft_lock:
+    def on_draw(self):
+        self.clear()
+
+        with self._analyzer.fft_lock:
             if self.y_axis_mode == 'lin':
-                y_coordinates = self.analyzer.fft
+                y_coordinates = self._analyzer.fft
             elif self.y_axis_mode == 'log':
                 # TODO: Fix division-by-zero RuntimeWarnings.
-                y_coordinates = 20 * np.log10(self.analyzer.fft)
+                y_coordinates = 20 * np.log10(self._analyzer.fft)
             else:
                 raise Exception('Unknown option: {0}'.format(self.x_axis_mode))
 
@@ -68,10 +63,23 @@ class SpectrumVisualizer(Visualizer):
                     if self.max_observed_y > 0.0:
                         y_coordinates /= self.max_observed_y
 
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
-            glColor3f(0.25, 0.0, 1.0)
-            glBegin(GL_LINE_STRIP)
+            gl.glColor3f(0.25, 0.0, 1.0)
+            gl.glBegin(gl.GL_LINE_STRIP)
             for x, y in zip(self.x_coordinates, y_coordinates):
-                glVertex3f(x, y * self.height, 0.0)
-            glEnd()
-            glutSwapBuffers()
+                gl.glVertex3f(x, y * self.height, 0.0)
+            gl.glEnd()
+
+
+@visualizer(name='spectrum')
+class SpectrumVisualizer(Visualizer):
+    VISUALIZER_WINDOW_TYPE = SpectrumVisualizerWindow
+    WINDOW_TITLE = 'Spectrum Visualizer'
+
+    def setup_analyzer(self):
+        self._analyzer = FFTAnalyzer(pulseaudio_client=self._pulseaudio_client,
+                                     sample_size=4096)
+
+    def start(self, **kwargs):
+        pyglet.clock.schedule_interval(self._window.update, 1 / 10)
+        super().start(**kwargs)
+
