2bd5156659833b5761cdcad99fd0f70068781e81
Patrick Bartels <pckbls@gmail.com>
2017-07-05 17:56:11 +0200

    Make waveform visualizer much faster

--- pulseviz/visualizers/waveform.py
+++ pulseviz/visualizers/waveform.py
@@ -1,27 +1,28 @@
 import pyglet
 from pyglet import gl
+import numpy
 from . import visualizer, Visualizer, VisualizerWindow
 from ..dsp.sampler import Sampler
 
 
 class WaveformVisualizerWindow(VisualizerWindow):
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._y_values = numpy.zeros(self._analyzer.sample_size)
+        self._bin_width = 1.0 / self._analyzer.sample_size
+
     def update(self, dt):
-        # TODO: Update vertex data here.
-        pass
+        with self._analyzer.samples_lock:
+            self._y_values = self._analyzer.samples / 2.0 + 0.5
 
     def on_draw(self):
         self.clear()
 
-        with self._analyzer.samples_lock:
-            bin_width = self.width / self._analyzer.sample_size
-
-            gl.glColor3f(0.25, 0.0, 1.0)
-            gl.glBegin(gl.GL_LINE_STRIP)
-            for i, height in enumerate(self._analyzer.samples):
-                x_min = i * bin_width
-                y = self._analyzer.samples[i] * self.height / 2 + self.height / 2
-                gl.glVertex3f(x_min, y, 0.0)
-            gl.glEnd()
+        gl.glColor3f(0.25, 0.0, 1.0)
+        gl.glBegin(gl.GL_LINE_STRIP)
+        for i in range(0, self._analyzer.sample_size):
+            gl.glVertex3f(i * self._bin_width * self.width, self._y_values[i] * self.height, 0.0)
+        gl.glEnd()
 
         self.draw_debug_overlay()
 
