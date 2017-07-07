70fe2b984261f6d021a3f092ad3962bbef59de70
Patrick Bartels <pckbls@gmail.com>
2017-07-07 10:01:10 +0200

    Add pulseaudio_server package

---
+++ tests/pulseaudio_server.py
@@ -0,0 +1,55 @@
+import subprocess
+import time
+
+
+def start():
+    """Starts a PulseAudio server."""
+
+    subprocess.run([
+        'pulseaudio',
+        '-n',
+        '-F', 'tests/data/minimal_server.pa',
+        '--exit-idle-time', '1337',
+        '--start',
+        '-vvvv'
+    ])
+
+    assert is_running()
+
+
+def stop(kill=False):
+    """Stops (or alternatively kills) all PulseAudio servers."""
+
+    if kill:
+        subprocess.run(['killall', '-9', 'pulseaudio'])
+    else:
+        subprocess.run(['pulseaudio', '--kill'])
+
+    time.sleep(3.0)  # TODO
+
+    assert not is_running()
+
+
+def is_running():
+    """Returns True if there's at least one PulseAudio server running."""
+
+    process = subprocess.run(['pulseaudio', '--check'])
+    return process.returncode == 0
+
+
+def load_module_null_sink(sink_name):
+    """Loads a module-null-sink with the given sink name."""
+
+    process = subprocess.run([
+        'pacmd',
+        'load-module',
+        'module-null-sink',
+        'sink_name="{0}"'.format(sink_name)
+    ])
+    assert process.returncode == 0
+
+
+def unload_module_null_sink():
+    """Unloads all module-null-sink modules."""
+
+    subprocess.run(['pacmd', 'unload-module', 'module-null-sink'])
