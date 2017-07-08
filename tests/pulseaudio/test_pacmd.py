f6c40ee70299b8a688827505251e05f899b68698
Patrick Bartels <pckbls@gmail.com>
2017-07-08 23:07:29 +0200

    Re-implement tests for pulseaudio package

--- tests/pulseaudio/test_pacmd.py
+++ tests/pulseaudio/test_pacmd.py
@@ -1,4 +1,20 @@
+import pytest
 from pulseviz.pulseaudio import pacmd
+
+
+def test_list_sources_pacmd_not_found(monkeypatch):
+    import subprocess
+
+    original_Popen = subprocess.Popen
+
+    def Popen(args, **kwargs):
+        args[0] = 'pacmd_does_not_exist_on_the_system'
+        return original_Popen(args, **kwargs)
+
+    monkeypatch.setattr(subprocess, 'Popen', Popen)
+
+    with pytest.raises(pacmd.PACmdException):
+        pacmd.list_sources()
 
 
 def test_list_sources(fixture_null_sink):
--- tests/pulseaudio/test_simple_client.py
+++ tests/pulseaudio/test_simple_client.py
@@ -1,92 +1,151 @@
-import threading
-import queue
-import time
-import random
 import pytest
+import ctypes
 from pulseviz.pulseaudio import simple_client
-
-
-# TODO: Test different sample rates
-# TODO: Test different channel settings (mono, stereo)
-
-
-@pytest.fixture()
-def fixture_simple_client(fixture_pulseaudio_server, request):
-    return simple_client.SimpleClient(name='pulseviz-tests',
-                                      stream_name=request.function.__name__)
 
 
 @pytest.fixture()
 def fixture_simple_record_client(fixture_null_sink, request):
     _, source_name = fixture_null_sink
-    return simple_client.SimpleRecordClient(source=source_name,
-                                            name='pulseviz-tests',
-                                            stream_name=request.function.__name__)
+    kwargs = {
+        'source': source_name,
+        'name': 'pulseviz-tests',
+        'stream_name': request.function.__name__,
+        'sample_frequency': 44100,
+        'sample_format': simple_client.SampleFormat.PA_SAMPLE_U8,
+        'channels': 1,
+        'target_latency': 10 * 1000 * 1000
+    }
+    return kwargs
 
 
 @pytest.fixture()
 def fixture_simple_playback_client(fixture_null_sink, request):
     sink_name, _ = fixture_null_sink
-    return simple_client.SimplePlaybackClient(sink=sink_name,
-                                              name='pulseviz-tests',
-                                              stream_name=request.function.__name__)
+    kwargs = {
+        'sink': sink_name,
+        'name': 'pulseviz-tests',
+        'stream_name': request.function.__name__,
+        'sample_frequency': 44100,
+        'sample_format': simple_client.SampleFormat.PA_SAMPLE_U8,
+        'channels': 1
+    }
+    return kwargs
 
 
-@pytest.mark.xfail(raises=simple_client.SimpleClientErrorException)
-def test_simple_client_failure(fixture_simple_client):
-    c = fixture_simple_client
-    with c:
-        pass
+def test_libpulse_simple_not_loadable(monkeypatch):
+    def LoadLibrary(_):
+        raise OSError('foobar')
+
+    monkeypatch.setattr(ctypes.cdll, 'LoadLibrary', LoadLibrary)
+
+    with pytest.raises(ImportError):
+        simple_client._load_libpulse_simple()
+
+    # Now undo the changes and reload the library properly, otherwise some weird side effects may occur.
+    monkeypatch.undo()
+    simple_client._load_libpulse_simple()
 
 
-def test_simple_record_client_enter_exit(fixture_simple_record_client):
-    c = fixture_simple_record_client
-    with c:
-        pass
+def test_connect_disconnect(fixture_simple_record_client, fixture_simple_playback_client):
+    src = simple_client.SimpleRecordClient(**fixture_simple_record_client)
+    spc = simple_client.SimplePlaybackClient(**fixture_simple_playback_client)
+    for c in [src, spc]:
+        assert not c.is_connected()
+        with c:
+            assert c.is_connected()
+        assert not c.is_connected()
 
 
-def test_simple_record_client_get_latency(fixture_simple_record_client):
-    c = fixture_simple_record_client
-    with c:
-        latency = c.get_latency()
+def test_call_methods_when_not_connected(fixture_simple_record_client, fixture_simple_playback_client):
+    src = simple_client.SimpleRecordClient(**fixture_simple_record_client)
+    with pytest.raises(simple_client.SimpleClientException): src.get_latency()  # noqa
+    with pytest.raises(simple_client.SimpleClientException): src.flush()  # noqa
+    with pytest.raises(simple_client.SimpleClientException): src.read(1)  # noqa
+
+    spc = simple_client.SimplePlaybackClient(**fixture_simple_playback_client)
+    with pytest.raises(simple_client.SimpleClientException): spc.get_latency()  # noqa
+    with pytest.raises(simple_client.SimpleClientException): spc.flush()  # noqa
+    with pytest.raises(simple_client.SimpleClientException): spc.write([0.0])  # noqa
+    with pytest.raises(simple_client.SimpleClientException): spc.drain()  # noqa
+
+
+def test_get_latency(fixture_simple_record_client, fixture_simple_playback_client):
+    src = simple_client.SimpleRecordClient(**fixture_simple_record_client)
+    with src:
+        latency = src.get_latency()
         assert latency > 0
 
+    spc = simple_client.SimplePlaybackClient(**fixture_simple_playback_client)
+    with spc:
+        latency = spc.get_latency()
+        assert latency == 0
 
-def test_simple_record_client_read(fixture_simple_record_client):
-    c = fixture_simple_record_client
+
+@pytest.mark.parametrize('sample_frequency', [44100])
+@pytest.mark.parametrize('sample_format', [
+    simple_client.SampleFormat.PA_SAMPLE_U8,
+    # simple_client.SampleFormat.PA_SAMPLE_S16LE,  # TODO: Enable
+    simple_client.SampleFormat.PA_SAMPLE_FLOAT32LE
+])
+@pytest.mark.parametrize('channels', [1, 2])
+def test_read(fixture_simple_record_client, sample_frequency, sample_format, channels):
+    fixture_simple_record_client['sample_frequency'] = sample_frequency
+    fixture_simple_record_client['sample_format'] = sample_format
+    fixture_simple_record_client['channels'] = channels
+
+    if channels != 1:
+        with pytest.raises(simple_client.SimpleClientException):
+            c = simple_client.SimpleRecordClient(**fixture_simple_record_client)
+    else:
+        c = simple_client.SimpleRecordClient(**fixture_simple_record_client)
+        with c:
+            for size in [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]:
+                data = c.read(size=size)
+                assert len(data) == size
+                assert data[0] is not None
+
+
+def test_write_and_drain(fixture_simple_playback_client):
+    c = simple_client.SimplePlaybackClient(**fixture_simple_playback_client)
     with c:
-        for size in [1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048]:
-            data = c.read(size=size)
-            assert len(data) == size
-            assert type(data) is list
-
-
-def test_simple_playback_client_write(fixture_simple_playback_client):
-    c = fixture_simple_playback_client
-    with c:
-        # TODO: 1 and 2 do not work.
+        # TODO: 1 and 2 do not work for some reason. Test this?
         for size in [4, 8, 16, 32, 64, 128, 256, 512, 1024]:
             data = [0 for i in range(0, size)]
             assert len(data) == size
             c.write(data)
 
-
-@pytest.mark.skip(reason='Not implemented yet.')
-def test_simple_record_client_no_server(fixture_simple_record_client):
-    assert False
+        c.drain()
 
 
-@pytest.mark.skip(reason='Not implemented yet.')
-def test_simple_playback_client_no_server(fixture_simple_playback_client):
-    assert False
+def test_no_server_available(fixture_pulseaudio_server_running,
+                             fixture_simple_playback_client, fixture_simple_record_client):
+    server = fixture_pulseaudio_server_running
+    src = simple_client.SimpleRecordClient(**fixture_simple_record_client)
+    spc = simple_client.SimplePlaybackClient(**fixture_simple_playback_client)
+
+    server.stop()
+    for c in [spc, src]:
+        with pytest.raises(simple_client.PulseAudioException):
+            with c: pass  # noqa
 
 
-@pytest.mark.skip(reason='Not implemented yet.')
-def test_simple_record_client_kill_server(fixture_simple_record_client):
-    assert False
+def test_simple_record_client_kill_server_while_connected(fixture_pulseaudio_server_running,
+                                                          fixture_simple_record_client):
+    server = fixture_pulseaudio_server_running
+    src = simple_client.SimpleRecordClient(**fixture_simple_record_client)
+
+    with src:
+        server.stop(kill=True)
+        with pytest.raises(simple_client.PulseAudioException):
+            src.flush()
 
 
-@pytest.mark.skip(reason='Not implemented yet.')
-def test_simple_playback_client_kill_server(fixture_simple_playback_client):
-    assert False
+def test_simple_playback_client_kill_server_while_connected(fixture_pulseaudio_server_running,
+                                                            fixture_simple_playback_client):
+    server = fixture_pulseaudio_server_running
+    spc = simple_client.SimplePlaybackClient(**fixture_simple_playback_client)
 
+    with spc:
+        server.stop(kill=True)
+        with pytest.raises(simple_client.PulseAudioException):
+            spc.flush()
