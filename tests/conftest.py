5cdf4a43c7d0930be130cb17e82b2281dc164de4
Patrick Bartels <pckbls@gmail.com>
2017-07-08 23:07:22 +0200

    Add --minimal-pulseaudio-server option

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,13 @@ from . import pulseaudio_server
 from .signal_generator import SignalGenerator
 
 
+def pytest_addoption(parser):
+    parser.addoption('--minimal-pulseaudio-server',
+                     action='store_true',
+                     default=None,
+                     help='If set, only a bare minimum of PulseAudio modules is loaded required for testing.')
+
+
 @pytest.fixture(scope='session', autouse=True)
 def fixture_pulseaudio_server():
     """
