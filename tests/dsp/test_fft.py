cb6bc96eca7c9588362fc949beed99017b0b025d
Patrick Bartels <pckbls@gmail.com>
2017-07-12 18:55:22 +0200

    Add test_frequencies_and_psd_length

--- tests/dsp/test_fft.py
+++ tests/dsp/test_fft.py
@@ -16,3 +16,13 @@ def test_analyze(fixture_fake_simple_client, window_function, output):
         time.sleep(1.0)
 
     assert analyzer.exit_success
+
+
+def test_frequencies_and_psd_length():
+    analyzer = FFT(sample_size=2048,
+                   source_name='foobar',
+                   stream_name='pulseviz-tests')
+
+    assert analyzer.frequencies[0] == 0.0
+    assert len(analyzer.frequencies) == len(analyzer.values)
+
