e434d692fbdf3395a0bbe985b1ce1ce53f0fc3f7
Patrick Bartels <pckbls@gmail.com>
2017-07-25 11:30:30 +0200

    Fix dummy visualizer

--- pulseviz/visualizers/dummy.py
+++ pulseviz/visualizers/dummy.py
@@ -4,8 +4,10 @@ from ..dsp.sampler import Sampler
 
 @visualizer(name='dummy')
 class DummyVisualizer(Visualizer):
+    ANALYZER_TYPE = Sampler
     WINDOW_TITLE = 'Dummy Visualizer'
 
-    def setup_analyzer(self, source_name):
-        self._analyzer = Sampler(source_name=source_name,
-                                 sample_size=2048)
+    def _setup_analyzer(self):
+        self._analyzer_kwargs['sample_size'] = 256
+        self._analyzer_kwargs['buffer_size'] = 4096
+        super()._setup_analyzer()
