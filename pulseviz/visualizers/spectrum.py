36ce89083c296fa19a46020b0932304028fd9d9f
Patrick Bartels <pckbls@gmail.com>
2017-07-25 11:30:40 +0200

    Update spectrum visualizer
    * Make it both much simpler and faster
    * Use hanning window by default
    * Plot the power spectral density instead of raw FFT values
    * Use (sample_size, buffer_size) = (256, 4096) as parameters for FFT
    * Remove unnecessary code
    * Minor changes

--- pulseviz/visualizers/spectrum.py
+++ pulseviz/visualizers/spectrum.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy
 import pyglet
 from pyglet import gl
 from . import visualizer, Visualizer, VisualizerWindow
@@ -6,82 +6,57 @@ from ..dsp.fft import FFT
 
 
 class SpectrumVisualizerWindow(VisualizerWindow):
-    def __init__(self, **kwargs):
+    def __init__(self, fft_size, y_lim, **kwargs):
         super().__init__(**kwargs)
-        self.auto_scale = True
-        self.max_observed_y = None
+        self._fft_size = fft_size
+        self._vertices = -numpy.inf * numpy.ones(fft_size * 2)
+        self._y_lim = y_lim
 
-        self.setup_x_axis('log')
-        self.setup_y_axis('log', -120, 0)
-
-    def setup_x_axis(self, mode):
-        self.x_axis_mode = mode
-
-    def setup_y_axis(self, mode, min_y, max_x):
-        self.y_axis_mode = mode
-        self.y_axis_min = min_y
-        self.y_axis_max = max_x
-
-    def update(self, dt):
-        # TODO: Update vertex data here.
-        pass
+    def update(self, values, dt):
+        self._vertices[1::2] = (values - self._y_lim[0]) / (self._y_lim[1] - self._y_lim[0]) * self.height
 
     def on_resize(self, width, height):
         super().on_resize(width, height)
 
-        if self.x_axis_mode == 'lin':
-            self.x_coordinates = np.linspace(0, self.width, len(self._analyzer.values))
-        elif self.x_axis_mode == 'log':
-            self.x_coordinates = self.width + 1
-            self.x_coordinates -= np.logspace(np.log2(self.width + 1),
-                                              0,
-                                              len(self._analyzer.values),
-                                              base=2)
-        else:
-            raise Exception('Unknown option: {0}'.format(self.x_axis_mode))
+        self._vertices[0::2] = self.width + 1
+        self._vertices[0::2] -= numpy.logspace(numpy.log2(self.width + 1),
+                                               0,
+                                               self._fft_size,
+                                               base=2)
 
     def on_draw(self):
         self.clear()
-
-        with self._analyzer.lock:
-            if self.y_axis_mode == 'lin':
-                y_coordinates = self._analyzer.values
-            elif self.y_axis_mode == 'log':
-                # TODO: Fix division-by-zero RuntimeWarnings.
-                y_coordinates = 20 * np.log10(self._analyzer.values)
-            else:
-                raise Exception('Unknown option: {0}'.format(self.x_axis_mode))
-
-            y_coordinates = (y_coordinates - self.y_axis_min) / (self.y_axis_max - self.y_axis_min)
-
-            # TODO: Re-implement this properly.
-            if False:
-                if self.auto_scale:
-                    m = np.max(y_coordinates)
-                    if self.max_observed_y is None or m > self.max_observed_y:
-                        self.max_observed_y = m
-                    if self.max_observed_y > 0.0:
-                        y_coordinates /= self.max_observed_y
-
-            gl.glColor3f(0.25, 0.0, 1.0)
-            gl.glBegin(gl.GL_LINE_STRIP)
-            for x, y in zip(self.x_coordinates, y_coordinates):
-                gl.glVertex3f(x, y * self.height, 0.0)
-            gl.glEnd()
-
+        gl.glColor3f(0.25, 0.0, 1.0)
+        pyglet.graphics.draw(self._fft_size,
+                             pyglet.gl.GL_LINE_STRIP,
+                             ('v2f', self._vertices))
         self.draw_debug_overlay()
 
 
 @visualizer(name='spectrum')
 class SpectrumVisualizer(Visualizer):
+    ANALYZER_TYPE = FFT
     VISUALIZER_WINDOW_TYPE = SpectrumVisualizerWindow
     WINDOW_TITLE = 'Spectrum Visualizer'
 
-    def setup_analyzer(self, source_name):
-        self._analyzer = FFT(source_name=source_name,
-                             window_function='hanning',
-                             sample_size=1024)
+    def _setup_analyzer(self):
+        self._analyzer_kwargs['sample_frequency'] = 44100
+        self._analyzer_kwargs['sample_size'] = 256
+        self._analyzer_kwargs['buffer_size'] = 4096
+        self._analyzer_kwargs['window_function'] = 'hanning'
+        self._analyzer_kwargs['output'] = 'psd'
+        self._analyzer_kwargs['scaling'] = 'log'
+        super()._setup_analyzer()
+
+    def _setup_window(self):
+        self._window_kwargs['fft_size'] = len(self._analyzer.values)
+        self._window_kwargs['y_lim'] = (-120.0, 0.0)
+        super()._setup_window()
+
+    def _update(self, dt):
+        with self._analyzer.lock:
+            self._window.update(self._analyzer.values, dt)
 
     def start(self, **kwargs):
-        pyglet.clock.schedule_interval(self._window.update, 1 / 60)
+        pyglet.clock.schedule_interval(self._update, 1 / 60)
         super().start(**kwargs)
