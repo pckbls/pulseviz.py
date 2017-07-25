e679fe17671b7b001305e29bebc3513ff65c8af7
Patrick Bartels <pckbls@gmail.com>
2017-07-25 11:21:15 +0200

    Update Sampler
    * Add buffer and buffer_size properties
    * Access sample_size property instead of private member
    * Raise PulsevizException instead of generic Exception

--- pulseviz/dsp/sampler.py
+++ pulseviz/dsp/sampler.py
@@ -1,19 +1,27 @@
 import threading
 import numpy
 from . import PulseAudioSignalAnalayzer
+from .. import PulsevizException
 
 
 class Sampler(PulseAudioSignalAnalayzer):
     """
-    Samples the audio signal recorded from the PulseAudio source.
+    Samples the audio signal recorded from the PulseAudio source
+    and optionally appends the samples to a fixed-size buffer.
     """
 
-    def __init__(self, sample_size, **kwargs):
+    def __init__(self, sample_size, buffer_size=None, **kwargs):
         super().__init__(**kwargs)
 
         self._samples_lock = threading.Lock()
-        self._sample_size = sample_size
-        self._samples = numpy.zeros(self._sample_size, dtype='f')
+        self._samples = numpy.zeros(sample_size, dtype='f')
+
+        if buffer_size is None:
+            self._buffer = None
+        elif buffer_size >= sample_size:
+            self._buffer = numpy.zeros(buffer_size, dtype='f')
+        else:
+            raise PulsevizException('buffer_size is too small: {0} < {1}'.format(buffer_size, sample_size))
 
     @property
     def lock(self):
@@ -21,13 +29,32 @@ class Sampler(PulseAudioSignalAnalayzer):
 
     @property
     def sample_size(self):
-        return self._sample_size
+        return len(self._samples)
 
     @property
     def samples(self):
         return self._samples
 
+    @property
+    def buffer(self):
+        if self._buffer is None:
+            raise PulsevizException('buffer_size has not been set.')
+
+        return self._buffer
+
+    @property
+    def buffer_size(self):
+        if self._buffer is None:
+            raise PulsevizException('buffer_size has not been set.')
+
+        return len(self._buffer)
+
     def _sample(self):
-        samples = self._pulseaudio_client.read(size=self._sample_size)
+        samples = self._pulseaudio_client.read(size=self.sample_size)
         with self.lock:
             self._samples[:] = samples
+
+            if self._buffer is not None:
+                n = self.buffer_size - self.sample_size
+                self._buffer[0:n] = self._buffer[self.sample_size:]
+                self._buffer[n:] = samples
